0568a05d08be7caf6e50a0e99868a688e53e48e5
no0758 <dandan.l060@shulidata.com>
2018-10-15 16:56:54 +0800

    cuda支持

--- train.py
+++ train.py
@@ -6,31 +6,36 @@ from torchvision import models
 import torch
 
 train_loader = data_loader(True)
-resnet50 = models.resnet50(num_classes=61)
+cuda = torch.cuda.is_available()
+if cuda:
+    resnet50 = models.resnet50(num_classes=61).cuda()
+else:
+    resnet50 = models.resnet50(num_classes=61)
 criterion = nn.CrossEntropyLoss()
-
-
 optimizer = optim.Adam(params=resnet50.parameters(),lr=0.01,weight_decay=1e-4)
-
 epochs = 60
 average_loss_series = []
+
+
 for epoch in range(epochs):
 
     running_loss = 0.0
 
     for i, data in enumerate(train_loader):
         inputs, labels = data
+        inputs = inputs.cuda() if cuda else inputs
+        labels = labels.cuda() if cuda else labels
         optimizer.zero_grad()
         outputs = resnet50(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-        if i % 10 == 9: #每10个batch打印一次训练状态
-            average_loss = running_loss/10
+        if i % 100 == 99: #每100个batch打印一次训练状态
+            average_loss = running_loss/100
             print("[{0},{1}] loss:  {2}".format(epoch+1, i+1, average_loss))
             average_loss_series.append(average_loss)
             running_loss = 0.0
 
 state = {'net':resnet50.state_dict(), 'optimizer':optimizer.state_dict(), 'epoch':epoch}
-torch.save(state, 'model')
+torch.save(state, 'model.ckpt')
