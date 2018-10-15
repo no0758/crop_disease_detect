030ea51fecd4d9e2db0d7ee1f0e34943305863e8
no0758 <dandan.l060@shulidata.com>
2018-10-15 13:50:16 +0800

    更新

--- utils.py
+++ utils.py
@@ -8,9 +8,9 @@ from torchvision import transforms as tfs
 def data_loader(train):
 
     train_tfs = tfs.Compose([
-            tfs.Resize(120),  #随机比例缩放
-            tfs.RandomHorizontalFlip(),  #随机的水平和竖直方向翻转
-            tfs.RandomCrop(96),  #随机位置截取
+            # tfs.Resize(120),  #随机比例缩放
+            # tfs.RandomHorizontalFlip(),  #随机的水平和竖直方向翻转
+            # tfs.RandomCrop(96),  #随机位置截取
             tfs.ColorJitter(brightness=0.5, contrast=0.5, hue=0.5),  #亮度、对比度和颜色的变化
             tfs.ToTensor(),
             tfs.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
